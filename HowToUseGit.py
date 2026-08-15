5dedcb5e1dcc4cbcc4e67601d4acf0f24fdb6435
Simon Diez <simon.diez@utp.edu.co>
2026-08-15 18:56:22 -0500

    Avance ejercicios de Python

---
+++ HowToUseGit.py
@@ -0,0 +1,10 @@
+"""
+git pull (actualiza cambios desde el repositorio remoto a tu repositorio local)
+git add . (prepara los cambios)
+git commit -m "mensaje de commit"
+git push (sube los cambios al repositorio remoto)
+
+git add .
+git commit -m "Avance ejercicios de Python"
+git push
+"""
