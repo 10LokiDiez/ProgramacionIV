27ebb163a9958c46fceeedafb7a173abd3895882
Simon Diez <simon.diez@utp.edu.co>
2026-08-17 00:30:15 -0500

    typecasting

---
+++ CURSO_PYTHON/typecasting.py
@@ -0,0 +1,14 @@
+name = "simon"
+inte = 23
+stri = 23.4
+
+
+print(f"name es un {type(name)}")
+print(f"inte es un {type(inte)}")
+
+stri = int(stri)
+print(stri)
+
+inte = float(inte)
+
+print(inte)
