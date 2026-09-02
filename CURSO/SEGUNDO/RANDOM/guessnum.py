b06bed551be37ec9c35ad79ba7bee5ef3b8210da
Simon Diez <simon.diez@utp.edu.co>
2026-09-01 21:19:46 -0500

    Avance ejercicios de Python

--- CURSO/SEGUNDO/RANDOM/guessnum.py
+++ CURSO/SEGUNDO/RANDOM/guessnum.py
@@ -1,2 +1,14 @@
 import random
 
+# print(help(random)) Para ver lo que hace
+
+num = random.randint(1,100)
+print(num)
+
+options =("rock", "paper", "scissors")
+op = random.choice(options)
+print(op)
+
+cards = ["2","3","4","5","6","7","8","9","10","J","Q","K","A",]
+random.shuffle(cards)
+print(cards)
